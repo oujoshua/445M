578c2e896c6297d49934696091bf6d6bceec70ab
Neil Klug <Neil.Klug@gmail.com>
2013-04-04 20:00:07 -0500

    UART is now interrupt based. There is a backup file just in case.

--- Lab6_test/UART.c
+++ Lab6_test/UART.c
@@ -1,8 +1,43 @@
+// UART2.c
+// Runs on LM3S1968
+// Use UART0 to implement bidirectional data transfer to and from a
+// computer running HyperTerminal.  This time, interrupts and FIFOs
+// are used.
+// Daniel Valvano
+// October 9, 2011
+// Modified by EE345L students Charlie Gough && Matt Hawk
+// Modified by EE345M students Agustinus Darmawan && Mingjie Qiu
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to the Arm Cortex M3",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2011
+
+   Program 5.11 Section 5.6, Program 3.10
+
+ Copyright 2011 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// U0Rx (VCP receive) connected to PA0
+// U0Tx (VCP transmit) connected to PA1
+
+#include "OS.h"
 #include "shell.h"
 #include "UART.h"
 #include <stdio.h>
 #include <string.h>
 
+#define NVIC_EN0_INT5           0x00000020  // Interrupt 5 enable
+#define NVIC_EN0_R              (*((volatile unsigned long *)0xE000E100))  // IRQ 0 to 31 Set Enable Register
+#define NVIC_PRI1_R             (*((volatile unsigned long *)0xE000E404))  // IRQ 4 to 7 Priority Register
 #define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420))
 #define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C))
 #define UART0_DR_R              (*((volatile unsigned long *)0x4000C000))
@@ -11,50 +46,160 @@
 #define UART0_FBRD_R            (*((volatile unsigned long *)0x4000C028))
 #define UART0_LCRH_R            (*((volatile unsigned long *)0x4000C02C))
 #define UART0_CTL_R             (*((volatile unsigned long *)0x4000C030))
+#define UART0_IFLS_R            (*((volatile unsigned long *)0x4000C034))
+#define UART0_IM_R              (*((volatile unsigned long *)0x4000C038))
+#define UART0_RIS_R             (*((volatile unsigned long *)0x4000C03C))
+#define UART0_ICR_R             (*((volatile unsigned long *)0x4000C044))
+#define UART_FR_RXFF            0x00000040  // UART Receive FIFO Full
 #define UART_FR_TXFF            0x00000020  // UART Transmit FIFO Full
 #define UART_FR_RXFE            0x00000010  // UART Receive FIFO Empty
 #define UART_LCRH_WLEN_8        0x00000060  // 8 bit word length
 #define UART_LCRH_FEN           0x00000010  // UART Enable FIFOs
 #define UART_CTL_UARTEN         0x00000001  // UART Enable
+#define UART_IFLS_RX1_8         0x00000000  // RX FIFO >= 1/8 full
+#define UART_IFLS_TX1_8         0x00000000  // TX FIFO <= 1/8 full
+#define UART_IM_RTIM            0x00000040  // UART Receive Time-Out Interrupt
+                                            // Mask
+#define UART_IM_TXIM            0x00000020  // UART Transmit Interrupt Mask
+#define UART_IM_RXIM            0x00000010  // UART Receive Interrupt Mask
+#define UART_RIS_RTRIS          0x00000040  // UART Receive Time-Out Raw
+                                            // Interrupt Status
+#define UART_RIS_TXRIS          0x00000020  // UART Transmit Raw Interrupt
+                                            // Status
+#define UART_RIS_RXRIS          0x00000010  // UART Receive Raw Interrupt
+                                            // Status
+#define UART_ICR_RTIC           0x00000040  // Receive Time-Out Interrupt Clear
+#define UART_ICR_TXIC           0x00000020  // Transmit Interrupt Clear
+#define UART_ICR_RXIC           0x00000010  // Receive Interrupt Clear
 #define SYSCTL_RCGC1_R          (*((volatile unsigned long *)0x400FE104))
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 #define SYSCTL_RCGC1_UART0      0x00000001  // UART0 Clock Gating Control
 #define SYSCTL_RCGC2_GPIOA      0x00000001  // port A Clock Gating Control
 
-//------------UART_InChar------------
-// Wait for new serial port input
-// Initialize the UART for 115,200 baud rate (assuming 50 MHz UART clock),
-// 8 bit word length, no parity bits, one stop bit, no FIFOs enabled
-// Input: none
-// Output: none
+void RxFifo_Init(void);
+int RxFifo_Put(char data);
+char RxFifo_Get(char* data);
+long RxFifo_Size(void);
+void TxFifo_Init(void);
+int TxFifo_Put(char data);
+char TxFifo_Get(char* data);
+long TxFifo_Size(void);
+
+#define FIFOSIZE   16         // size of the FIFOs (must be power of 2)
+#define FIFOSUCCESS 1         // return value on success
+#define FIFOFAIL    0         // return value on failure
+                              // create index implementation FIFO (see FIFO.h)
+
+OS_SemaphoreType RxFifoMutex, RxFifoDataAvail, RxFifoDataRoomLeft;
+OS_SemaphoreType TxFifoDataAvail, TxFifoMutex, TxFifoDataRoomLeft;
+char *RxFifoPutPt, *RxFifoGetPt;
+char *TxFifoPutPt;
+char *TxFifoGetPt;
+char RxFifo[FIFOSIZE];
+char TxFifo[FIFOSIZE];
+// Initialize UART0
+// Baud rate is 115200 bits/sec
 void UART_Init(void){
+  volatile long delay;
+  OS_InitSemaphore(&TxFifoDataAvail,0);
+  OS_InitSemaphore(&TxFifoDataRoomLeft, FIFOSIZE);
+  OS_InitSemaphore(&TxFifoMutex, 1);
+  OS_InitSemaphore(&RxFifoDataAvail,0);
+  OS_InitSemaphore(&RxFifoDataRoomLeft, FIFOSIZE);
+  OS_InitSemaphore(&RxFifoMutex, 1);
   SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0; // activate UART0
+  delay = SYSCTL_RCGC1_R;
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA; // activate port A
+  delay = SYSCTL_RCGC2_R;
+  RxFifo_Init();                        // initialize empty FIFOs
+  TxFifo_Init();
   UART0_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
   UART0_IBRD_R = 27;                    // IBRD = int(50,000,000 / (16 * 115,200)) = int(27.1267)
   UART0_FBRD_R = 8;                     // FBRD = int(0.1267 * 64 + 0.5) = 8
                                         // 8 bit word length (no parity bits, one stop bit, FIFOs)
   UART0_LCRH_R = (UART_LCRH_WLEN_8|UART_LCRH_FEN);
+  UART0_IFLS_R &= ~0x3F;                // clear TX and RX interrupt FIFO level fields
+                                        // configure interrupt for TX FIFO <= 1/8 full
+                                        // configure interrupt for RX FIFO >= 1/8 full
+  UART0_IFLS_R += (UART_IFLS_TX1_8|UART_IFLS_RX1_8);
+                                        // enable TX and RX FIFO interrupts and RX time-out interrupt
+  UART0_IM_R |= (UART_IM_RXIM|UART_IM_TXIM|UART_IM_RTIM);
   UART0_CTL_R |= UART_CTL_UARTEN;       // enable UART
   GPIO_PORTA_AFSEL_R |= 0x03;           // enable alt funct on PA1-0
   GPIO_PORTA_DEN_R |= 0x03;             // enable digital I/O on PA1-0
+                                        // UART0=priority 2
+  NVIC_PRI1_R = (NVIC_PRI1_R&0xFFFF00FF)|0x00004000; // bits 13-15
+  NVIC_EN0_R |= NVIC_EN0_INT5;          // enable interrupt 5 in NVIC
 }
+// copy from hardware RX FIFO to software RX FIFO
+// stop when hardware RX FIFO is empty or software RX FIFO is full
+void static copyHardwareToSoftware(void){
+  char letter;
+  while(((UART0_FR_R&UART_FR_RXFE) == 0) && (RxFifo_Size() < (FIFOSIZE - 1))){
+    letter = UART0_DR_R;
+    RxFifo_Put(letter);
+    OS_Signal(&RxFifoDataAvail);
+  }
+}
+// copy from software TX FIFO to hardware TX FIFO
+// stop when software TX FIFO is empty or hardware TX FIFO is full
+void static copySoftwareToHardware(void){
+  char letter;
+  while(((UART0_FR_R&UART_FR_TXFF) == 0) && (TxFifo_Size() > 0)){
 
-//------------UART_InChar------------
-// Wait for new serial port input
-// Input: none
-// Output: ASCII code for key typed
-unsigned char UART_InChar(void){
-  while((UART0_FR_R&UART_FR_RXFE) != 0) ;
-  return((unsigned char)(UART0_DR_R&0xFF));
+    TxFifo_Get(&letter);
+    UART0_DR_R = letter;
+    OS_Signal(&TxFifoDataRoomLeft);
+  }
 }
-//------------UART_OutChar------------
-// Output 8-bit to serial port
-// Input: letter is an 8-bit ASCII character to be transferred
-// Output: none
+// input ASCII character from UART
+// spin if RxFifo is empty
+unsigned char UART_InChar(void){
+   //Semaphore locks acquisition
+     char letter;
+  OS_Wait(&RxFifoDataAvail);
+  OS_Wait(&RxFifoMutex);
+  while(RxFifo_Get(&letter) == FIFOFAIL){};
+   //Semaphore lock release
+  OS_Signal(&RxFifoMutex);
+  return(letter);
+}
+// output ASCII character to UART
+// spin if TxFifo is full
 void UART_OutChar(unsigned char data){
-  while((UART0_FR_R&UART_FR_TXFF) != 0);
-  UART0_DR_R = data;
+     //Semaphore locks
+  OS_Wait(&TxFifoDataRoomLeft);
+  OS_Wait(&TxFifoMutex);
+  while(TxFifo_Put(data) == FIFOFAIL){};
+  UART0_IM_R &= ~UART_IM_TXIM;          // disable TX FIFO interrupt
+  copySoftwareToHardware();
+  UART0_IM_R |= UART_IM_TXIM;           // enable TX FIFO interrupt
+  OS_Signal(&TxFifoMutex);
+}
+// at least one of three things has happened:
+// hardware TX FIFO goes from 3 to 2 or less items
+// hardware RX FIFO goes from 1 to 2 or more items
+// UART receiver has timed out
+void UART0_Handler(void){
+  if(UART0_RIS_R&UART_RIS_TXRIS){       // hardware TX FIFO <= 2 items
+    UART0_ICR_R = UART_ICR_TXIC;        // acknowledge TX FIFO
+    // copy from software TX FIFO to hardware TX FIFO
+    copySoftwareToHardware();
+    if(TxFifo_Size() == 0){             // software TX FIFO is empty
+      UART0_IM_R &= ~UART_IM_TXIM;      // disable TX FIFO interrupt
+    }
+  }
+  if(UART0_RIS_R&UART_RIS_RXRIS){       // hardware RX FIFO >= 2 items
+    UART0_ICR_R = UART_ICR_RXIC;        // acknowledge RX FIFO
+    // copy from hardware RX FIFO to software RX FIFO
+    copyHardwareToSoftware();
+  }
+  if(UART0_RIS_R&UART_RIS_RTRIS){       // receiver timed out
+    UART0_ICR_R = UART_ICR_RTIC;        // acknowledge receiver time out
+    // copy from hardware RX FIFO to software RX FIFO
+    copyHardwareToSoftware();
+
+  }
 }
 
 
@@ -192,6 +337,7 @@ void UART_OutUHex(unsigned long number){
 // Input: pointer to empty buffer, size of buffer
 // Output: Null terminated string
 // -- Modified by Agustinus Darmawan + Mingjie Qiu --
+
 void UART_InString(char *bufPt, unsigned short max) {
 	int length = 0;
 	char character;
@@ -222,4 +368,170 @@ void UART_InString(char *bufPt, unsigned short max) {
     character = UART_InChar();
   }
   *bufPt = 0;
+}
+
+
+
+// ******** RxFifo_Init ************
+// Initialize the Fifo to be empty
+// Inputs: size
+// Outputs: none 
+// In Lab 2, you can ignore the size field
+// In Lab 3, you should implement the user-defined fifo size
+// In Lab 3, you can put whatever restrictions you want on size
+//    e.g., 4 to 64 elements
+//    e.g., must be a power of 2,4,8,16,32,64,128
+void RxFifo_Init(void){
+  //long sr;
+  //sr = StartCritical();      // make atomic
+  RxFifoPutPt = RxFifoGetPt = &RxFifo[0]; // Empty
+  //OS_InitSemaphore(&RxFifoMutex, 1);
+  //OS_InitSemaphore(&RxFifoDataAvail, 0);
+  //OS_InitSemaphore(&RxFifoDataRoomLeft, FIFOSIZE);
+  //EndCritical(sr);
+}
+
+// ******** RxFifo_Put ************
+// Enter one data sample into the Fifo
+// Called from the background, so no waiting 
+// Inputs:  data
+// Outputs: true if data is properly saved,
+//          false if data not saved, because it was full
+// Since this is called by interrupt handlers 
+//  this function can not disable or enable interrupts
+int RxFifo_Put(char data){
+  char *nextPutPt;
+  //OS_Wait(&OSFifoDataRoomLeft);
+  nextPutPt = RxFifoPutPt + 1;
+  if(nextPutPt == &RxFifo[FIFOSIZE]){
+    nextPutPt = &RxFifo[0];
+  }
+  if(nextPutPt == RxFifoGetPt){
+    return 0;
+  }
+  *(RxFifoPutPt) = data;
+  RxFifoPutPt = nextPutPt;
+  //OS_Signal(&RxFifoMutex);
+ // OS_Signal(&RxFifoDataAvail);
+  return 1;
+}
+
+// ******** RxFifo_Get ************
+// Remove one data sample from the Fifo
+// Called in foreground, will spin/block if empty
+// Inputs:  none
+// Outputs: data 
+char RxFifo_Get(char* data){
+  char retVal;
+ // OS_Wait(&RxFifoDataAvail);
+  //OS_Wait(&RxFifoMutex);
+  if(RxFifoGetPt == RxFifoPutPt){
+return 0;
+  }
+  retVal = *(RxFifoGetPt++);
+  *(data) = retVal;
+  if(RxFifoGetPt == &RxFifo[FIFOSIZE]){
+    RxFifoGetPt = &RxFifo[0];
+  }
+  //OS_Signal(&RxFifoMutex);
+  //OS_Signal(&OSFifoDataRoomLeft);
+  return retVal;
+}
+
+// ******** RxFifo_Size ************
+// Check the status of the Fifo
+// Inputs: none
+// Outputs: returns the number of elements in the Fifo
+//          greater than zero if a call to OS_Fifo_Get will return right away
+//          zero or less than zero if the Fifo is empty 
+//          zero or less than zero  if a call to OS_Fifo_Get will spin or block
+
+long RxFifo_Size(void){
+  if(RxFifoPutPt < RxFifoGetPt){
+    return ((long)(RxFifoPutPt-RxFifoGetPt+(FIFOSIZE*sizeof(char)))/sizeof(char));
+  }
+  return ((long)(RxFifoPutPt-RxFifoGetPt)/sizeof(char));
+}
+
+// ******** TxFifo_Init ************
+// Initialize the Fifo to be empty
+// Inputs: size
+// Outputs: none 
+// In Lab 2, you can ignore the size field
+// In Lab 3, you should implement the user-defined fifo size
+// In Lab 3, you can put whatever restrictions you want on size
+//    e.g., 4 to 64 elements
+//    e.g., must be a power of 2,4,8,16,32,64,128
+void TxFifo_Init(void){
+  //long sr;
+  //sr = StartCritical();      // make atomic
+  TxFifoPutPt = TxFifoGetPt = &TxFifo[0]; // Empty
+  //OS_InitSemaphore(&TxFifoMutex, 1);
+  //OS_InitSemaphore(&TxFifoDataAvail, 0);
+  //OS_InitSemaphore(&TxFifoDataRoomLeft, FIFOSIZE);
+  //EndCritical(sr);
+}
+
+// ******** TxFifo_Put ************
+// Enter one data sample into the Fifo
+// Called from the background, so no waiting 
+// Inputs:  data
+// Outputs: true if data is properly saved,
+//          false if data not saved, because it was full
+// Since this is called by interrupt handlers 
+//  this function can not disable or enable interrupts
+int TxFifo_Put(char data){
+  char *nextPutPt;
+  //OS_Wait(&OSFifoDataRoomLeft);
+  //OS_Wait(&OSFifoMutex);
+  nextPutPt = TxFifoPutPt + 1;
+  if(nextPutPt == &TxFifo[FIFOSIZE]){
+    nextPutPt = &TxFifo[0];
+  }
+  if(nextPutPt == TxFifoGetPt){
+    return 0;
+  }
+  *(TxFifoPutPt) = data;
+//  *(TxFifoPutPt)=15;// try to see contents
+  TxFifoPutPt = nextPutPt;
+  //OS_Signal(&TxFifoMutex);
+ // OS_Signal(&TxFifoDataAvail);
+  return 1;
+}
+
+// ******** TxFifo_Get ************
+// Remove one data sample from the Fifo
+// Called in foreground, will spin/block if empty
+// Inputs:  none
+// Outputs: data 
+char TxFifo_Get(char* data){
+  char retVal;
+ // OS_Wait(&TxFifoDataAvail);
+  //OS_Wait(&TxFifoMutex);
+  if(TxFifoGetPt == TxFifoPutPt){
+    return 0;
+  }
+  retVal = *(TxFifoGetPt++);
+  *(data)=retVal;
+  if(TxFifoGetPt == &TxFifo[FIFOSIZE]){
+    TxFifoGetPt = &TxFifo[0];
+  }
+  
+  //OS_Signal(&TxFifoMutex);
+  //OS_Signal(&OSFifoDataRoomLeft);
+  return retVal;
+}
+
+// ******** TxFifo_Size ************
+// Check the status of the Fifo
+// Inputs: none
+// Outputs: returns the number of elements in the Fifo
+//          greater than zero if a call to OS_Fifo_Get will return right away
+//          zero or less than zero if the Fifo is empty 
+//          zero or less than zero  if a call to OS_Fifo_Get will spin or block
+long TxFifo_Size(void){
+  if(TxFifoPutPt < TxFifoGetPt){
+    return ((long)(TxFifoPutPt-TxFifoGetPt+(FIFOSIZE*sizeof(char)))/sizeof(char));
+  }
+  return ((long)(TxFifoPutPt-TxFifoGetPt)/sizeof(char));
 }
