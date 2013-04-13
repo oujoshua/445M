fb03a827cb2f2c00192658d26e511b77eef1d845
Sam Caldwell <samcaldwell@utexas.edu>
2013-04-13 15:17:27 -0500

    adding board1 files

---
+++ RaceCondition/board1_driver/board1_main.c
@@ -0,0 +1,12 @@
+#include "OS.h"
+#include "shell.h"
+
+// board controlling the two motors
+
+int main (void) {
+  OS_Init();
+  
+  
+  
+  OS_Launch(TIME_2MS);
+}
---
+++ RaceCondition/board1_driver/using.h
@@ -0,0 +1,7 @@
+
+#define USING_ADC 1
+#define USING_IR 0
+#define USING_TACHO 0
+#define USING_PING 0
+#define USING_PWM 1
+
