ed6ec30d8e6a3012e89e4d69a59dff50698f6929
Sam Caldwell <samcaldwell@utexas.edu>
2013-04-28 00:41:54 -0500

    made a Fuzzy_Distance data type that has three variables: close, far, and ok

--- src/fuzzy.c
+++ src/fuzzy.c
@@ -1,2 +1,14 @@
 
 #include "fuzzy.h"
+
+Fuzzy_Distance Fuzzy_IRs[4], Fuzzy_Ping;
+
+// convert a crisp input into a fuzzy distance
+// input  : distance in centimeters
+// input  : pointer to Fuzzy_Distance variable to write the result to
+// outputs: none (writes fuzzy values to struct passed in via pointer)
+void Fuzzify(unsigned long dist_cm, Fuzzy_Distance *fuzzy) {
+  fuzzy->close = 64;
+  fuzzy->ok = 64;
+  fuzzy->far = 64;
+}
---
+++ src/fuzzy.h
@@ -0,0 +1,7 @@
+
+typedef struct Fuzzy_Distance {
+  unsigned char close;
+  unsigned char ok;
+  unsigned char far;
+} Fuzzy_Distance;
+
