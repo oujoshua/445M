fa266c72cb1dce929e731ecd82c025dcd5f74f9e
Sam Caldwell <samcaldwell@utexas.edu>
2013-04-28 01:00:06 -0500

    adding fuzzy operations and & or

--- src/fuzzy.c
+++ src/fuzzy.c
@@ -3,16 +3,31 @@
 
 FuzzyDistance Fuzzy_IRs[4], Fuzzy_Ping;
 
-// convert a crisp input into a fuzzy distance
-// input  : distance in centimeters
-// input  : pointer to FuzzyDistance variable to write the result to
-// outputs: none (writes fuzzy values to struct passed in via pointer)
+/* convert a crisp input into a fuzzy distance
+ * input  : dist_cm distance in centimeters
+ * input  : fuzzy pointer to FuzzyDistance variable to write the result to
+ * outputs: none (writes fuzzy values to struct passed in via pointer)
+ */
 void Fuzzify(unsigned long dist_cm, FuzzyDistance *fuzzy) {
   fuzzy->close = 64;
   fuzzy->ok = 64;
   fuzzy->far = 64;
 }
 
-unsigned char Fuzzy_Subtract(FuzzyValue A, FuzzyValue B) {
-  return 0;
+/* compute the fuzzy or, A + B, which is equivalent to a mathematical MAX
+ * input A : FuzzyValue argument to or/max
+ * input B : FuzzyValue argument to or/max
+ * output  : FuzzyVale or/max of A and B
+ */
+FuzzyValue Fuzzy_Or(FuzzyValue A, FuzzyValue B) {
+  return (A > B) ? A : B;
+}
+
+/* compute the fuzzy and, A * B, which is equivalent to a mathematical MIN
+ * input A : FuzzyValue argument to and/min
+ * input B : FuzzyValue argument to and/min
+ * output  : FuzzyValue and/min of A and B
+ */
+FuzzyValue Fuzzy_And(FuzzyValue A, FuzzyValue B) {
+  return (A < B) ? A : B;
 }
--- src/fuzzy.h
+++ src/fuzzy.h
@@ -7,3 +7,24 @@ typedef struct FuzzyDistance {
   FuzzyValue far;
 } FuzzyDistance;
 
+/* convert a crisp input into a fuzzy distance
+ * input  : dist_cm distance in centimeters
+ * input  : fuzzy pointer to FuzzyDistance variable to write the result to
+ * outputs: none (writes fuzzy values to struct passed in via pointer)
+ */
+void Fuzzify(unsigned long dist_cm, FuzzyDistance *fuzzy);
+
+/* compute the fuzzy or, A + B, which is equivalent to a mathematical MAX
+ * input A : FuzzyValue argument to or/max
+ * input B : FuzzyValue argument to or/max
+ * output  : FuzzyVale or/max of A and B
+ */
+FuzzyValue Fuzzy_Or(FuzzyValue A, FuzzyValue B);
+
+/* compute the fuzzy and, A * B, which is equivalent to a mathematical MIN
+ * input A : FuzzyValue argument to and/min
+ * input B : FuzzyValue argument to and/min
+ * output  : FuzzyValue and/min of A and B
+ */
+FuzzyValue Fuzzy_And(FuzzyValue A, FuzzyValue B);
+
